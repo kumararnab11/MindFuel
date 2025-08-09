985e22bc8aad0ce0483dac2f428053122641f239
Max-31 <nathrajarshi25@gmail.com>
2025-08-09 23:56:52 +0530

    ft.: Upgrade to React-hook-form

---
+++ client/src/components/core/Input1.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+
+const Input = React.forwardRef( function Input(props, ref){
+    const {label, placeholder, type} = props;
+    return (
+        <div>
+            <label className="block text-sm font-medium text-gray-50 mb-1">
+                {label} <span className="text-red-500">*</span>
+            </label>
+
+            <input 
+                type={type} 
+                className="bg-richblack-700 placeholder-pure-greys-300 w-full px-4 py-2 border border-b-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                placeholder={placeholder}
+                ref= {ref}
+                {...props}
+            />
+        </div>
+    )
+})
+
+export default Input;
+
+// const Input = ({ label, placeholder, inputValue, setInputValue,type }) => {
+//   return (
+//     <div >
+//       <label className="block text-sm font-medium text-gray-50 mb-1">
+//         {label} <span className="text-red-500">*</span>
+//       </label>
+//       <input
+//         type={type}
+//         className="bg-richblack-700
+//         placeholder-pure-greys-300 w-full px-4 py-2 border
+//         border-b-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+//         placeholder={placeholder}
+//         value={inputValue}
+//         onChange={(e) => setInputValue(e.target.value)}
+//       />
+//     </div>
+//   );
+// };

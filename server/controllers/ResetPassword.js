e98eecde2b3850dcccdd1f55f5f71b4ec0369341
kumararnab11 <kumararnab0342@gmail.com>
2025-08-13 21:35:52 +0530

    reset password weblink fixed

--- server/controllers/ResetPassword.js
+++ server/controllers/ResetPassword.js
@@ -29,7 +29,7 @@ exports.resetPasswordToken = async (req, res) => {
         );
 
         //create url
-        const url = `http://localhost:3000/update-password/${token}`;
+        const url = `${process.env.FRONTEND_URL}/update-password/${token}`;
         //send mail containing the url
         await mailSender(email, "Password Reset Link", `Password Reset Link: ${url}`);
 

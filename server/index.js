75d3dc47b9c93686bd0c2982ac23756148d3c746
kumararnab11 <kumararnab0342@gmail.com>
2025-08-14 18:29:37 +0530

    backend url acces to localhost also

--- server/index.js
+++ server/index.js
@@ -24,7 +24,10 @@ app.use(express.json());
 app.use(cookieparser());
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: [
+      process.env.FRONTEND_URL,
+      "http://localhost:5173",
+    ],
     credentials: true,
   })
 );
